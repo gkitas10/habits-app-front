50105173158b8467d908314c1846c2e4f2ee7a3a
Jorge <gkitas10@gmail.com>
2022-05-05 14:20:15 -0500

    Add props to TutorialStep component
    Adds img, step, followup props to TutorialStep component and use them
    in the component

--- src/components/TutorialStep.js
+++ src/components/TutorialStep.js
@@ -1,12 +1,16 @@
-import Start from '../assets/tutorial/start.png';
 import '../styles/TutorialStep.css';
 
-const TutorialStep = () => {
+const TutorialStep = ({ img, step, followup }) => {
     return ( 
         <div className="tutorial-step">
-            <div>Paso 1. Da click en comienza ya para dirigirte al formulario</div>
+            <div className='tutorial-step__step-instruc'>{ step }</div>
+            { followup && (
+                <div className='tutorial-step__followup'>
+                    { followup }
+                </div>
+            )}
             <div className="tutorial-step__img-cont">
-                <img className='tutorial-step__img' src={Start}/>
+                <img className='tutorial-step__img' src={ img }/>
             </div>
         </div>
      );
