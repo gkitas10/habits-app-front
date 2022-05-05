fe9ad75f7a29f548dacc51e8db1270b32325d90d
Jorge <gkitas10@gmail.com>
2022-05-05 17:32:12 -0500

    Create colorcode component
    Creates colorcode component and adss style to it
    Creates css module

---
+++ src/components/ColorCode.js
@@ -0,0 +1,59 @@
+import styles from '../styles/ColorCode.module.css';
+
+const ColorCode = () => {
+    return ( 
+        <div className={styles['color-code']}>
+            <div className={styles.row+' '+styles['titles-row']}>
+                <div className={styles.title+' '+styles['title-color']}>
+                    Color
+                </div>
+                <div className={styles.title+' '+styles['title-range']}>
+                    Rango de rendimiento
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles['color-cont']}>
+                    <div className={styles['verylow-performance']+' '+styles['color']}></div>
+                </div>
+                <div className={styles['color-code__performance-range']}>
+                    40% o menos
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles['color-cont']}>
+                    <div className={styles['low-performance']+' '+styles['color']}></div>
+                </div>
+                <div className={styles['color-code__performance-range']}>
+                    De 41% a 60%
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles['color-cont']}>
+                    <div className={styles['reg-performance']+' '+styles['color']}></div>
+                </div>
+                <div className={styles['color-code__performance-range']}>
+                    De 61% a 80%
+                </div>
+            </div>
+
+            <div className={styles.row}>
+                <div className={styles['color-cont']}>
+                    <div className={styles['good-performance']+' '+styles['color']}></div>
+                </div>
+                <div className={styles['color-code__performance-range']}>
+                    De 81% a 90%
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles['color-cont']}>
+                    <div className={styles['great-performance']+' '+styles['color']}></div>
+                </div>
+                <div className={styles['color-code__performance-range']}>
+                    De 91% a 100%
+                </div>
+            </div>          
+        </div>
+     );
+}
+ 
+export default ColorCode;
