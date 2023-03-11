359df87fea7920a13f378c28eb851332b6cbb873
unknown <gkitas10@gmail.com>
2023-03-10 18:47:45 -0600

    Change backendURL in axios

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let baseURL = '';
 
 if(process.env.NODE_ENV === 'production'){
-    baseURL = 'https://habits-app-back.herokuapp.com' 
+    baseURL = 'https://habitsapp-back.onrender.com' 
 }else {
     baseURL = 'http://localhost:4000'
 }
